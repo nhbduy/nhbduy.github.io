6cfd0129b00208f4db3ae95efcfbbdafccdbdea5
Duy Nguyen <duy.nguyen@christinas.vn>
2019-09-30 23:50:54 +0700

    Updates

---
+++ precache-manifest.3280ef9c5f242e0b51b73db178923b0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0b3bdb56388cac02f87cea9ff48db6",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "329fdc6973335656f6a8",
+    "url": "/resume/static/css/main.eaacfba6.chunk.css"
+  },
+  {
+    "revision": "5d0f0b3c86d7694fbb7c",
+    "url": "/resume/static/js/2.f12f51b1.chunk.js"
+  },
+  {
+    "revision": "329fdc6973335656f6a8",
+    "url": "/resume/static/js/main.8ec460bf.chunk.js"
+  },
+  {
+    "revision": "842c9e958140f0ac0346",
+    "url": "/resume/static/js/runtime-main.7dfd1280.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.fd4bb1dae462dfe3f83e0b9d98a092f8.js"
+  "/resume/precache-manifest.3280ef9c5f242e0b51b73db178923b0f.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/resume/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
