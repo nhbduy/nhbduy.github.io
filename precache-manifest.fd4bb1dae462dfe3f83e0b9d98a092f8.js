e1c39f23c38f27f68346401cecd785ce1e57eb52
Duy Nguyen <duy.nguyen@christinas.vn>
2019-09-30 21:41:35 +0700

    Updates

---
+++ precache-manifest.fd4bb1dae462dfe3f83e0b9d98a092f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.699f89d9.js"
+  },
+  {
+    "revision": "ef4dabd88fe802bcfbdc",
+    "url": "/resume/static/js/main.ef4dabd8.chunk.js"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "/resume/static/js/2.9c5ca2f4.chunk.js"
+  },
+  {
+    "revision": "ef4dabd88fe802bcfbdc",
+    "url": "/resume/static/css/main.c393aa72.chunk.css"
+  },
+  {
+    "revision": "4fbc132ec5555e93934cf8bccba1bd0c",
+    "url": "/resume/index.html"
+  }
+];
